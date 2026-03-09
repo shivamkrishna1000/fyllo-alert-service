db841788c9e1579724950b1356ac8059d3c62bad
Shivam Krishna <50731434+shivamkrishna1000@users.noreply.github.com>
2026-03-09 16:11:46 +0530

    Wati Integrated

--- app/config.py
+++ app/config.py
@@ -25,3 +25,27 @@ def get_farm_user_id() -> str:
 def get_fyllo_password() -> str:
     """Retrieve Fyllo password from environment."""
     return os.environ.get("FYLLO_PASSWORD")
+
+def get_wati_base_url() -> str | None:
+    """Retrieve Wati base URL from environment"""
+    return os.environ.get("WATI_BASE_URL")
+
+
+def get_wati_tenant_id() -> str | None:
+    """Retrieve Wati tenant ID from environment."""
+    return os.environ.get("WATI_TENANT_ID")
+
+
+def get_wati_api_token() -> str | None:
+    """Retrieve Wati API Token from environment."""
+    return os.environ.get("WATI_API_TOKEN")
+
+
+def get_wati_test_number() -> str | None:
+    """Retrieve Test Mobile No. from environment."""
+    return os.environ.get("WATI_TEST_NUMBER")
+
+
+def get_wati_template_name() -> str | None:
+    """Retrieve Wati Template Name from environment."""
+    return os.environ.get("WATI_TEMPLATE_NAME")
--- app/notification_service.py
+++ app/notification_service.py
@@ -1,21 +1,60 @@
 from typing import Dict
 from app.database import insert_sent_notification
+from app.alert_processor import simplify_alert_text
+import requests
+
+from app.config import (
+    get_wati_base_url,
+    get_wati_tenant_id,
+    get_wati_api_token,
+    get_wati_test_number,
+    get_wati_template_name
+)
 
 def send_notification(connection, alert: Dict, farmer: Dict) -> None:
-    """
-    Placeholder for WhatsApp (WATI) integration.
-    Currently just prints.
-    Later will call WATI API.
-    """
+
+    wati_base_url = get_wati_base_url()
+    wati_tenant_id = get_wati_tenant_id()
+    wati_api_token = get_wati_api_token()
+    wati_test_number = get_wati_test_number()
+    wati_template_name = get_wati_template_name()
+
+    url = f"{wati_base_url}/{wati_tenant_id}/api/v1/sendTemplateMessage"
+
+    headers = {
+        "Authorization": f"Bearer {wati_api_token}",
+        "Content-Type": "application/json"
+    }
+
     farmer_name = farmer.get("farmer_name")
     mobile_number = farmer.get("mobile_number")
     plot_id = alert.get("plotId")
     alert_id = alert.get("id")
+    raw_text = alert.get("text")
+    alert_text = simplify_alert_text(raw_text) if raw_text else " "
 
-    print("\n--- Sending Notification ---")
-    print(f"Plot ID: {plot_id}")
-    print(f"Date: {alert['date']}")
-    print(f"Message: {alert['text']}")
+    parameters = [
+        {"name": "1", "value": farmer_name or " "},
+        {"name": "2", "value": plot_id or "Your Plot"},
+        {"name": "3", "value": alert_text or " "}
+    ]
+
+    payload = {
+        "template_name": wati_template_name,
+        "broadcast_name": "fyllo_alert_test_run",
+        "parameters": parameters
+    }
+
+    request_url = f"{url}?whatsappNumber={wati_test_number}"
+
+    response = requests.post(
+        request_url,
+        json=payload,
+        headers=headers,
+        timeout=10
+    )
+
+    print("WATI Response:", response.status_code, response.text)
 
     insert_sent_notification(
         connection=connection,
@@ -23,5 +62,5 @@ def send_notification(connection, alert: Dict, farmer: Dict) -> None:
         farmer_name=farmer_name,
         mobile_number=mobile_number,
         plot_id=plot_id,
-        message=alert["text"],
+        message=alert_text,
     )
